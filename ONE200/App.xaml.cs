bac3501a2355d46206101d8511268398c8ca8906
Ganesh G <ganesh.g@version1.com>
2026-08-17 15:09:30 +0530

    Initial WPF project scaffold

---
+++ ONE200/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ONE200;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
